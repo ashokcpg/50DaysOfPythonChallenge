21e9e5ef152b39c29880ab671126c872356ff284
Aakrity Chapagai <aakritycpg@gmail.com>
2020-07-30 13:24:20 +0545

    First Project

---
+++ 1.AddThreeUserInputNumbers/program.py
@@ -0,0 +1,19 @@
+
+#Add three numbers with user input. 
+
+Number1 = input('Enter First Number :')
+Number2 = input('Enter Second Number :')
+Number3 = input('Enter Third Number :')
+
+#Add Three Numbers, 
+sum = float(Number1)+float(Number2)+float(Number3)
+ 
+ #Display the sum
+ 
+print('The sum of {0} , {1} and {2} is {3}'.format(Number1,Number2,Number3,sum))
+
+#output :
+#Enter First Number :980
+#Enter Second Number :909
+#Enter Third Number :10.5
+#The sum of 980 , 909 and 10.5 is 1899.5
