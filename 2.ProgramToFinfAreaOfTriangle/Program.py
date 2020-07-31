3d839d1ea1c82e4d6bce5d5b295fe77b536e8b94
Aakrity Chapagai <aakritycpg@gmail.com>
2020-07-31 10:58:59 +0545

    Second Program and snippet added

---
+++ 2.ProgramToFinfAreaOfTriangle/Program.py
@@ -0,0 +1,23 @@
+#Python program to find Area of triangle
+
+#Three sides of triangle a ,b and c are provided by the user
+
+First = float(input('Enter First Side : '))
+
+Second = float(input('Enter Second Side : '))
+
+Third = float(input('Enter Third Side : '))
+
+#Calculate The Semi-Parameter
+S = (First + Second + Third)/2
+
+ #Calculate the area
+
+Area = (S*(S-First)*(S-Second)*(S-Third))**0.5
+print('The Area Of The Triangle Is %0.2f' %Area)
+
+#output :
+#Enter First Side : 33
+#Enter Second Side : 22 
+#Enter Third Side : 11.1
+#The Area Of The Triangle Is 20.02
--- README.md
+++
@@ -1,5 +0,0 @@
-# 50DaysOfPythonChallenge
-
-| Day | #Title |
-| :---: | :---: |
-| 01 | Adding 3 Numbers |
