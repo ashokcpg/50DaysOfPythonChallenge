d7c58f701cfcf9932b7fd4d36127d80e3fe0ce49
Aakrity Chapagai <aakritycpg@gmail.com>
2020-08-01 11:33:37 +0545

    Third Program Added

---
+++ 3.AverageOfThreeNumbers/program.py
@@ -0,0 +1,18 @@
+#Program to find Average of Three Number
+
+firstNumber = float(input("Enter The first Number : "))
+secondNumber = float(input("Enter The second Number : "))
+thirdNumber = float(input("Enter The third Number : "))
+
+#To Calculate Average Of Three Number
+
+Avg = (firstNumber + secondNumber + thirdNumber)/3
+
+#display the Average
+
+print ('The Average of {0}, {1} and {2} is {3}'.format(firstNumber,secondNumber, thirdNumber,Avg))
+
+#print('The Average Of three numbers is :',Avg)
+
+#output
+
