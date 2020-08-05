ecd080bc17ce41bea3bc3fb3b2cccd5f77f81727
Aakrity Chapagai <aakritycpg@gmail.com>
2020-08-05 10:05:39 +0545

    Thirst Program and Snippet Added

--- 3.AverageOfThreeNumbers/program.py
+++ 3.AverageOfThreeNumbers/program.py
@@ -19,4 +19,5 @@ print ('The Average of {0}, {1} and {2} is {3}'.format(firstNumber,secondNumber,
 # Enter The first Number : 10    
 # Enter The second Number : 20
 # Enter The third Number : 60
+
 # The Average of 10.0, 20.0 and 60.0 is 30.0
