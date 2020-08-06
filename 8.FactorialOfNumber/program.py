fb1631900500fdce732bea3320f8e2eb80ff53a7
Aakrity Chapagai <aakritycpg@gmail.com>
2020-08-06 20:36:52 +0545

    8th Program and Snippet added

--- 8.FactorialOfNumber/program.py
+++ 8.FactorialOfNumber/program.py
@@ -7,7 +7,6 @@ Number = 11
 factorial = 1
 
 # check if the number is negative, positive or zero
-
 if Number< 0:
 
    print("The factorial of given number doesnot exist")
@@ -23,3 +22,16 @@ else:
 
 #    Output :
 # The factorial of 11 is 39916800
+
+
+
+# 2.
+# Note: To test the program for a different number, change the value of num.
+# Here, the number whose factorial is to be found is stored in num, 
+# and we check if the number is negative, zero or positive using if...elif...else statement. 
+# If the number is positive, we use for loop and range() function to calculate the factorial.
+
+# 1.
+# The factorial of a number is the product of all the integers from 1 to that number.
+# For example, the factorial of 6 is 1*2*3*4*5*6 = 720. 
+# Factorial is not defined for negative numbers, and the factorial of zero is one, 0! = 1.
