97e5c394dab79742f4b0eb053d540c81b4128b1e
Aakrity Chapagai <aakritycpg@gmail.com>
2020-08-08 14:48:23 +0545

    10th Program and Snippet Added

---
+++ 10.CheckPositiveNegativeaOrZero/program.py
@@ -0,0 +1,18 @@
+Number = float(input("Enter a number: "))
+if Number > 0:
+   print("Positive number")
+elif Number == 0:
+   print("Zero")
+else:
+   print("Negative number")
+
+   #output :
+   #Enter a number: 11
+    #Positive number
+
+    #Enter a number: 0
+       #Zero
+
+       #Enter a number: -5
+        #Negative number
+
