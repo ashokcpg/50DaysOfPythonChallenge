ff60b915ab3e2e653acadcbe00afdf206a01bd22
Aakrity Chapagai <aakritycpg@gmail.com>
2020-08-08 15:54:47 +0545

    10th Program and Snippet Added

--- 10.CheckPositiveNegativeaOrZero/program.py
+++ 10.CheckPositiveNegativeaOrZero/program.py
@@ -5,17 +5,14 @@ elif Number == 0:
    print("Zero")
 else:
    print("Negative number")
-   
+
    #output :
    #Enter a number: 11
-     #Positive number
-
-    #Enter a number: 0
-    #Zero
-
-       #Enter a number: -5
-        #Negative number
-
-        #Enter a number: 111.11
-        #Positive number
+                      #Positive number
+ #Enter a number: 0
+                    #Zero
+#Enter a number: -5
+                 #Negative number
+ #Enter a number: 111.11
+              #Positive number
 
