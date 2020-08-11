8ee15fa1c777286e7ff879f0d0bb859c96d89e2f
Aakrity Chapagai <aakritycpg@gmail.com>
2020-08-11 13:34:29 +0545

    12th Program Added

---
+++ 12.PythonFuctionBasics/program.py
@@ -0,0 +1,34 @@
+def simpleInterest(P,T,R):
+    # Function Body
+    return(P*T*R/100)
+
+principle = int(input("Enter the Principle :"))
+year = int(input("Enter the Year :"))
+rate = int(input("Enter the Rate :"))
+print("The interest is: ")
+interest = simpleInterest(principle,year,rate) #Function Call
+print(interest)
+
+
+
+
+
+
+
+
+
+
+
+
+# num = int(input("Enter a number: "))  
+# if (num % 2) == 0:
+#     print("The Number is Even")
+# else:
+#     print("Thr Number is odd")
+
+    # Enter a number: 1111      
+    # The Number is odd
+
+
+
+   
