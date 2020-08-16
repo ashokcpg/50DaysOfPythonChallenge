952d9c833c2d1c00786a0d206f4875a247de7410
Aakrity Chapagai <aakritycpg@gmail.com>
2020-08-16 13:59:08 +0545

    17th Program Files Added

---
+++ 17.SumOfSeriesWithCube/program.py
@@ -0,0 +1,14 @@
+def sumOfSeries(n):
+    sum = 0
+    for i in range(1, n+1):
+        sum +=i*i*i
+
+    return sum
+n = 8
+
+print(sumOfSeries(n))
+
+
+  # Input :1^3 + 2^3 + 3^3 + 4^3 + 5^3 + 6^3 + 7^3 + 8^3   
+ #Output :1296
+            
