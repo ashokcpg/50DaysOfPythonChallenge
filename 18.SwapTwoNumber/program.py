a8035adb933728dc10fde5ebbbc0065801f53b56
Aakrity Chapagai <aakritycpg@gmail.com>
2020-08-17 21:31:51 +0545

    18th Program Files Added

---
+++ 18.SwapTwoNumber/program.py
@@ -0,0 +1,19 @@
+#   python program to swap two numbers without using another variable
+
+x = 7
+y = 8
+
+print ("Before swapping: ")
+print("Value of x :",x,"and y:",y)
+
+#COD TO SWAP 'X' AND 'Y'
+
+print ("After swapping :")
+
+print("Value of x : ", x, " and y : ", y)
+
+#Output :
+# Before swapping: 
+# Value of x : 7 and y: 8
+# After swapping :
+# Value of x :  7  and y :  8
