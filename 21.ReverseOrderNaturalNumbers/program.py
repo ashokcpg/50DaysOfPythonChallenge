bfe20daf41b4adfd2358ace142427af3f6537dd0
Aakrity Chapagai <aakritycpg@gmail.com>
2020-08-21 16:43:15 +0545

    21th Program Files Added

---
+++ 21.ReverseOrderNaturalNumbers/program.py
@@ -0,0 +1,16 @@
+#Python program to Reverse Order Natural Number :
+
+Number = int(input("Enter any Number :"))
+i =  Number
+
+print("List of Numbers From {0} to 1 in Reverse Order :".format(Number))
+
+while (i >= 1):
+    print (i, end = '  ')
+    i = i - 1
+
+
+# Output :
+#     Enter any Number :18
+# List of Numbers From 18 to 1 in Reverse Order :
+# 18  17  16  15  14  13  12  11  10  9  8  7  6  5  4  3  2  1
