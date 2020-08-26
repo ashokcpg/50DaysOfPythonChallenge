7f4a557156279f28ddc57e9f446a0db785a9d5b4
Aakrity Chapagai <aakritycpg@gmail.com>
2020-08-26 13:55:10 +0545

    26th Program Files Added

---
+++ 26.CheckAlphabetOrNot/program.py
@@ -0,0 +1,16 @@
+Pyhton Program to check character is Alphabet or not 
+
+ch = input("Enter Your Character :")
+
+if((ch>='a' and ch<='z') or (ch>'A' and ch<='Z')) :
+    print("The GIven Character", ch,"is an Alphabet")
+
+else :
+         print("The Given Character", ch,"is Not an Alphabet")
+
+#  Output :
+# Enter Your Character :MaNgO
+# The GIven Character MaNgO is an Alphabet
+
+# Enter Your Character :123
+# The Given Character 123 is Not an Alphabet
