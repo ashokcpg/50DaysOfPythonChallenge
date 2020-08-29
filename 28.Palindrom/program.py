739276a46f4161dd9e312e177cbe620929fa0c7d
Aakrity Chapagai <aakritycpg@gmail.com>
2020-08-29 23:08:24 +0545

    28th Program Files Added

--- 28.Palindrom/program.py
+++ 28.Palindrom/program.py
@@ -1,29 +1,15 @@
-#Python program to print palindrome numbers from 1 to 100 using loop:
+largeNum = int(input("Max Number: "))
+smallNum = int(input("Small Number: "))
 
-maximum = int(input(" Enter the Maximum Value : "))
-minimum = int(input(" Enter the minimum Value : "))
+print(f"Palindrome Numbers between {smallNum} and {largeNum} are: ")
+for i in range(smallNum, largeNum+1):
+    num = str(i)
+    if (num[::-1] == num):
+        print(f"{num}", end = " ")
 
-print("Palindrome Numbers between %d and %d are : " %(minimum, maximum))
+# Output
 
-# OR :
-
-# print(f"Palindrome Numbers  Are : ")
-
-for num in range(1, maximum + 1):
-    temp = num
-    reverse = 0
-    
-    while(temp > 0):
-        Reminder = temp % 10
-        reverse = (reverse * 10) + Reminder
-        temp = temp //10
-
-    if(num == reverse):
-        print("%d " %num, end = '  ')
-
-    #  Output :
-
-# Enter the Maximum Value : 100
-#  Enter the minimum Value : 1
-# Palindrome Numbers between 1 and 100 are :
-# 1   2   3   4   5   6   7   8   9   11   22   33   44   55   66   77   88   99   
+# Max Number: 100
+# Small Number: 8
+# Palindrome Numbers between 8 and 100 are: 
+# 8 9 11 22 33 44 55 66 77 88 99
