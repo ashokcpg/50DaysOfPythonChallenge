8f983669633b1aeeab152681229395c0a53f6f1c
Aakrity Chapagai <aakritycpg@gmail.com>
2020-09-02 13:47:17 +0545

    Calculation Cube of a Number

---
+++ calculationCubeOfaNumber/main.py
@@ -0,0 +1,12 @@
+#Python Program to find cube of a Number
+
+Number = float(input("Enter any numeric Value :"))
+
+cube = Number*Number*Number
+
+print(f"The Cube of a Given Number is :",cube)
+
+
+# Output :
+# Enter any numeric Value :5
+# The Cube of a Given Number is : 125.0
