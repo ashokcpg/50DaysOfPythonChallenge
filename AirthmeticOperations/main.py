10aaaeedb40a15f3dcb721a2c6e4a05227cacc12
Aakrity Chapagai <aakritycpg@gmail.com>
2020-09-02 13:30:51 +0545

    Perform Airthmetic Operation

--- AirthmeticOperations/main.py
+++ AirthmeticOperations/main.py
@@ -1,4 +1,4 @@
-# Python Program to Perform Arithmetic Operations
+# Python Program to Perform Arithmetic Operations.
 
 num1 = float(input(" Enter the First  Number : "))
 num2 = float(input(" Enter the Second  Number : "))
@@ -21,9 +21,13 @@ mod = num1 % num2
 # Exponent of num1 and num2
 expo = num1 ** num2
 
-print("The Sum of {0} and {1} : {2}".format(num1, num2, add))
-print("The Subtraction of {0} from {1} : {2}".format(num2, num1, sub))
-print("The Multiplication of {0} and {1} : {2}".format(num1, num2, multi))
-print("The Division of {0} and {1} : {2}".format(num1, num2, div))
-print("The Modulus of {0} and {1} : {2}".format(num1, num2, mod))
-print("The Exponent Value of {0} and {1} : {2}".format(num1, num2, expo))
+#Output :
+# Enter the First  Number : 20
+#  Enter the Second  Number : 10
+
+# The Sum of 20.0 and 10.0 : 30.0
+# The Subtraction of 10.0 from 20.0 : 10.0
+# The Multiplication of 20.0 and 10.0 : 200.0
+# The Division of 20.0 and 10.0 : 2.0
+# The Modulus of 20.0 and 10.0 : 0.0
+# The Exponent Value of 20.0 and 10.0 : 10240000000000.0
