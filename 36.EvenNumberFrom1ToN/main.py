37c4d143087d91a34fbcee28ca5cfe7f27e25600
Aakrity Chapagai <aakritycpg@gmail.com>
2020-09-06 11:27:08 +0545

    36th Program Files Added

--- 36.EvenNumberFrom1ToN/main.py
+++ 36.EvenNumberFrom1ToN/main.py
@@ -1,9 +1,10 @@
 # Python Program to Print Even Numbers from 1 to N
+
 maximum = int(input("Enter the Maximum Value : "))
 for number in range(1, maximum+1):
     if(number % 2 == 0):
         print("{0}".format(number))
-
+        
 # Output :
 #Enter the Maximum Value : 20
 # 2 
