956154554c121fcb1229f0f2bf20c2f4683e5bf0
Aakrity Chapagai <aakritycpg@gmail.com>
2020-09-07 22:25:48 +0545

    38th Program Files Added

--- 38.FindSomeOfArray/main.py
+++ 38.FindSomeOfArray/main.py
@@ -1,12 +1,19 @@
+# Python Program  to find sum  of elements in given array 
 
+# driver function 
 arr = [] 
-  
+
+# input values to list 
 arr = [20, 10, 5, 1] 
   
+# sum() is an inbuilt function in python that adds  
+# all the elements in list,set and tuples and returns 
+# the value  
 
 ans = sum(arr) 
   
 print ('Sum of the array is ',ans) 
 
 #Output :
+
 # Sum of the array is  36
