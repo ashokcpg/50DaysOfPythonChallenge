107f9a5523629ad97ec39159e50ecca9098f8835
Aakrity Chapagai <aakritycpg@gmail.com>
2020-09-09 17:42:34 +0545

    41th Program Files Added

---
+++ 41.AsciiTable/main.py
@@ -0,0 +1,12 @@
+# Python Program to Print ASCII Table
+
+digit = 0
+
+while digit<=50:
+    print("%d = %c" %(digit, chr(digit)))
+    digit += 1
+
+
+# This program prints ASCII table in Python programming language
+#  using while loop and built-in function chr().
+# Function chr() returns a Unicode string of one character.
