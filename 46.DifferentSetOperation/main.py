7844b3f1c53870d30f9d6b2cf0405ed63e551887
Aakrity Chapagai <aakritycpg@gmail.com>
2020-09-14 22:11:46 +0545

    ksd

--- 46.DifferentSetOperation/main.py
+++ 46.DifferentSetOperation/main.py
@@ -13,9 +13,15 @@ print("Difference of B and A is",B - A)
 
 print("Symmetric difference of A and B is", A^B)
 
+
 # Output :
+
 # Union of A and B is {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 12, 14, 15}
+
 # Intersection of A and B is set()
 # Difference of A and B is {0, 3, 6, 9, 12, 15}
+
 # Difference of B and A is {1, 2, 4, 5, 7, 8, 14}
-# Symmetric difference of A and B is {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 12, 14, 15}
+
+# Symmetric difference of A and B is {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 12,
+#  14, 15}
