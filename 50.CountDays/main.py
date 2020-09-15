50e1f07206423dcd4e35cc74f78bdf08b038d511
Aakrity Chapagai <aakritycpg@gmail.com>
2020-09-15 19:17:42 +0545

    skf

---
+++ 50.CountDays/main.py
@@ -0,0 +1,10 @@
+#Python Program to calculate age in days from date of birth in Python.
+
+from datetime import date
+
+birth_date = date(2002, 1, 31)
+current_date = date(2020, 9, 15)
+
+delta = current_date - birth_date
+
+print("Number of days from your Birthdays = ",delta.days)
