b2e395d28a1faf8215d1f7b1dfe0c0a5c0dfa317
Aakrity Chapagai <aakritycpg@gmail.com>
2020-09-15 22:45:08 +0545

    Files Added

--- 52.FahrenheitToCelcius/main.py
+++ 52.FahrenheitToCelcius/main.py
@@ -2,3 +2,8 @@ fahrenheit = float(input("Enter temprature in  Fahrenheit : "))
 celsius = (fahrenheit - 32)*5/9
 
 print('%.2f Fahrenheit is =  %0.2f Celsius' %(fahrenheit, celsius))
+
+# Output : 
+
+# Enter temprature in  Fahrenheit : 101.12 
+# 101.12 Fahrenheit is =  38.40 Celsius
